c6d5decdbc40ee460841973d6c89d05f90378a68
Jerry Xie <jx.feedback@gmail.com>
2017-09-21 22:46:01 +0800

    Improve search function

--- application/views/material_list.php
+++ application/views/material_list.php
@@ -1,9 +1,16 @@
-<div style="margin-left:20px">
+<!-- <div style="margin-left:20px">
+    <form class="form-inline">
+        <label for="searchBox" class="sr-only">Search</label>
+        <input id="searchBox" type="text" class="form-control" placeholder="Keyword">
+        <button class="btn btn-default">Search</button>
+    </form>
+    <br />
     <table id="jqGrid"></table>
     <div id="jqGridPager"></div>
-</div>
+</div> -->
 <script type="text/javascript"> 
-$(document).ready(function () {
+$(document).ready( function () 
+{
     $.jgrid.defaults.width = 1000;
     $("#jqGrid").jqGrid({
         url: '<?=site_url('material/list_json');?>',
@@ -13,18 +20,36 @@ $(document).ready(function () {
         colModel: [
             { label: 'id', name: 'id', width: 75 },
             { label: 'Code', name: 'Code', width: 150 },
-            { label: 'Description', name: 'Description', width: 300 },
+            {
+                label: 'Description',
+                name: 'Description',
+                width: 300,
+                searchoptions: { sopt: ['cn'] },
+            },
             { label: 'UOM', name: 'UOM', width: 50 },
             { label: 'MinStock', name: 'MinStock', width: 50 },
         ],
         viewrecords: true,
         height: 500,
         rowNum: 30,
+        rowList: [20,30,50],
+        sortname: 'Code',
+        sortorder: "asc",
         pager: "#jqGridPager",
-        loadonce: true
+        loadonce: true,
+        multiSort: true,
     });
+    // activate the toolbar searching
+    $('#jqGrid').jqGrid(
+        'filterToolbar',
+        {
+            stringResult: true,
+            searchOperators: true,
+        }
+    );
     // add navigation bar with some built in actions for the grid
-    $('#jqGrid').navGrid('#jqGridPager',
+    $('#jqGrid').navGrid(
+        '#jqGridPager',
         {
             edit: false,
             add: false,
@@ -34,6 +59,19 @@ $(document).ready(function () {
             view: true,
             position: "left",
             cloneToTop: false
-        });
+        },
+        {}, // edit options
+        {}, // add options
+        {}, // delete options
+        {
+            multipleSearch: true,
+            multipleGroup: true, 
+            showQuery: true,
+            // set the names of the template
+            tmplNames: ["Template One", "Template Two"],
+            // set the template contents
+            // tmplFilters: [template1, template2],
+        }, // search options - define multiple search);
+    );
 });
 </script>
